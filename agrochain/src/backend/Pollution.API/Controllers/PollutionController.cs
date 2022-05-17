ad78040b104b1d8327df604dc47fdb6d39d170e0
Divyanshu Ranjan Srivastava <greatdrs@gmail.com>
2022-05-18 00:24:23 +0530

    added GET pollution history API

---
+++ agrochain/src/backend/Pollution.API/Controllers/PollutionController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Pollution.API.Models;
+
+namespace Pollution.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PollutionController : ControllerBase
+    {
+        private readonly ILogger<PollutionController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly string _apiKey;
+        private readonly string _baseUrl;
+        private readonly string _urlPath;
+
+        public PollutionController(ILogger<PollutionController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _apiKey = _configuration["APIKey"];
+            _baseUrl = _configuration["OpenWeatherURL"];
+            _urlPath = @"data/2.5/air_pollution/history";
+        }
+
+        //https://localhost:7081/api/Pollution?lat=50&lon=50&startDatetime=2020-11-27T20:21:00&endDatetime=2020-11-30T20:21:00
+        [HttpGet(Name = "GetPolltionHistory")]
+        //TODO: discuss the return type
+        public async Task<Response> GetPolltionHistory(double lat, double lon, string startDatetime, string endDatetime)
+        {
+            var startTimeInUnix = ConvertDateTimeToUnix(startDatetime);
+            var endTimeInUnix = ConvertDateTimeToUnix(endDatetime);
+            
+            var completeUrl = _baseUrl + _urlPath + $"?lat={lat}&lon={lon}&start={startTimeInUnix}&end={endTimeInUnix}&appid={_apiKey}";
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    using (var httpResponseMessage = await client.GetAsync(completeUrl))
+                    {
+                        httpResponseMessage?.EnsureSuccessStatusCode();
+                        var contents = await httpResponseMessage?.Content?.ReadAsStringAsync();
+
+                        var result = JsonConvert.DeserializeObject<Response>(contents);
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something went wrong while using http client");
+                throw;
+            }
+        }
+
+        private long ConvertDateTimeToUnix(string dateTime)
+        {
+            DateTime.TryParse(dateTime, out DateTime temp);
+            var unixTime = ((DateTimeOffset)temp).ToUnixTimeSeconds();
+            return unixTime;
+        }
+    }
+}
---
+++ agrochain/src/backend/Pollution.API/Models/Response.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+
+namespace Pollution.API.Models
+{
+    public partial class Response
+    {
+        [JsonProperty("coord")]
+        public Coord LocationCoordinates { get; set; }
+
+        [JsonProperty("list")]
+        public List[] PollutionData { get; set; }
+    }
+
+    public partial class Coord
+    {
+        [JsonProperty("lon")]
+        public long Lon { get; set; }
+
+        [JsonProperty("lat")]
+        public long Lat { get; set; }
+    }
+
+    [JsonObject]
+    public partial class List
+    {
+        [JsonProperty("main")]
+        public Main Main { get; set; }
+
+        [JsonProperty("components")]
+        public Components Pollutants { get; set; }
+
+        [JsonProperty("dt")]
+        public long DateTime { get; set; }
+    }
+
+    public partial class Main
+    {
+        [JsonProperty("aqi")]
+        public long AQI { get; set; }
+    }
+
+    public class Components
+    {
+        [JsonProperty("co")]
+        public double CO { get; set; }
+
+        [JsonProperty("no")]
+        public double NO { get; set; }
+
+        [JsonProperty("no2")]
+        public double NO2 { get; set; }
+
+        [JsonProperty("o3")]
+        public double O3 { get; set; }
+
+        [JsonProperty("so2")]
+        public double SO2 { get; set; }
+
+        [JsonProperty("pm2_5")]
+        public double PM25 { get; set; }
+
+        [JsonProperty("pm10")]
+        public double PM10 { get; set; }
+
+        [JsonProperty("nh3")]
+        public double NH3 { get; set; }
+    }
+}
