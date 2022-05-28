0e449a54b3e1f812ff7ca0c880236f65ae8196c1
ARNAB DAS <raj713335@gmail.com>
2022-05-28 23:20:22 +0530

    :wrench: Updated the NFT Details Page

--- agrochain/src/Components/NFTDetails.js
+++ agrochain/src/Components/NFTDetails.js
@@ -363,67 +363,114 @@ export const NFTDetails = () => {
         <Loading />
     )
     return (
-        <>
+        <React.Fragment>
         <div className="container mt-4">
             <br />
             <br />
             <div className="row">
                 <div className="col-md-6">
-                        <div className="card zoom">
-                        <div className="card-body">
-                            <div>
-                                <img className="img-fluid" src={nfts.image} />
-                            </div>
-                        </div>
+                    <div className="card zoom">
+                         <img className="img-fluid" src={nfts.image} />
                     </div>
                 </div>
-                    <div className="col-md-6">
-                        <div className="card">
-                            <div className="card-body">
-                                <div>
-                                    <h4 className="text-muted my-0 mt-4">{nfts.seller}</h4>
-                                    <h2 className="mt-0">{nfts.name}</h2>
+                <div className="col-md-6">
+                   <div className="mt-4 mt-md-0 mx-4">
+                       <h2 className="mb-0">{nfts.name}</h2>
+                       <h4 className="text-blue type-6 mb-0 mt-1"><span className="bold">Seller: </span>{nfts.seller}</h4>
+                       <br />
+                       <div className="row">
+                           <div className="col-4 col-md-4">
+                               <p className="my-0">
+                                   <span className="semi-bold">Name :</span>
+                               </p>
+                           </div>
+                           <div className="col-8 col-md-8">
+                               <p className="text-muted my-0">{farmername}</p>
+                           </div>
+                       </div>
+                       <div className="row">
+                           <div className="col-4 col-md-4">
+                               <p className="my-0">
+                                   <span className="semi-bold">Contact No.:</span>
+                               </p>
+                           </div>
+                           <div className="col-8 col-md-8">
+                               <p className="text-muted my-0">{contact}</p>
+                           </div>
+                       </div>
+                       <div className="row">
+                           <div className="col-4 col-md-4">
+                               <p className="my-0">
+                                   <span className="semi-bold">Aadhar No. :</span>
+                               </p>
+                           </div>
+                           <div className="col-8 col-md-8">
+                               <p className="text-muted my-0">{govtid}</p>
+                           </div>
+                       </div>
+                       <div className="row">
+                           <div className="col-4 col-md-4">
+                               <p className="my-0">
+                                   <span className="semi-bold">Co-ordinates :</span>
+                               </p>
+                           </div>
+                           <div className="col-8 col-md-8">
+                               <p className="text-muted my-0">{location}</p>
+                           </div>
+                       </div>
+                       <div className="row">
+                           <div className="col-4 col-md-4">
+                               <p className="my-0">
+                                   <span className="semi-bold">IOT Device ID :</span>
+                               </p>
+                           </div>
+                           <div className="col-8 col-md-8">
+                               <p className="text-muted my-0">{iotdeviceid}</p>
+                           </div>
+                       </div>
+                       <div className="row">
+                           <div className="col-4 col-md-4">
+                               <p className="my-0">
+                                   <span className="semi-bold">Description :</span>
+                               </p>
+                           </div>
+                           <div className="col-8 col-md-8">
+                               <p className="text-muted my-0">{nfts.description}</p>
+                           </div>
+                        </div>
+
+                        <br />
+                        <h4 className="text-success mb-0">Current Price:</h4>
+                        <h2 className="text-success my-0">
+                            <i className="fab fa-ethereum"></i> {ethers.utils.formatEther(nfts.totalPrice)}
+                        </h2>
+                        <h4 className="text-danger mt-1">
+                            Last <i className="fab fa-ethereum"></i> {ethers.utils.formatEther(nfts.totalPrice)}
+                        </h4>
+
+                        <br />
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="d-grid gap-2">
+                                    <button onClick={() => buyMarketItem(nfts)} className="btn btn-lg btn-primary">
+                                        <i className="fas fa-wallet fa-fw"></i> Confirm Buy
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="d-grid gap-2">
+                                    <button className="btn btn-lg btn-secondary">
+                                        <i className="fas fa-tag fa-fw"></i> Make an Offer
+                                    </button>
                                 </div>
                             </div>
                         </div>
-                    <br />
-                    <p className="text-muted">
-                        {nfts.description}
-                    </p>
-                        <p className="mt-3 text-grey"> <i className="fa fa-copyright" aria-hidden="true"> {farmername} </i> <i className="fa fa-id-card" aria-hidden="true"></i> {govtid} <i className="fa fa-address-book"></i> {contact} <i className="fa fa-compass" aria-hidden="true"></i> {location} <i className="fa fa-map-marker"></i> {iotdeviceid}</p>
-                    <br />
-                    <h4 className="text-success mb-0">Current Price:</h4>
-                    <h2 className="text-success my-0">
-                        <i className="fab fa-ethereum"></i> {ethers.utils.formatEther(nfts.totalPrice)}
-                    </h2>
-                    <h4 className="text-danger mt-1">
-                        Last <i className="fab fa-ethereum"></i> {ethers.utils.formatEther(nfts.totalPrice)}
-                    </h4>
-
-                    
-                    <br />
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div className="d-grid gap-2">
-                                <button onClick={() => buyMarketItem(nfts)} className="btn btn-lg btn-primary">
-                                    <i className="fas fa-wallet fa-fw"></i>
-                                    Confirm Buy
-                                </button>
-                            </div>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="d-grid gap-2">
-                                <button className="btn btn-lg btn-secondary">
-                                    <i className="fas fa-tag fa-fw"></i>
-                                    Make an Offer
-                                </button>
-                            </div>
-                        </div>
+                        <br />
+                        <br />
                     </div>
-                    <br />
-                    <br />
-
                     </div>
+                </div>
+                <div className="row">
 
 
                     <div className="row">
@@ -452,17 +499,7 @@ export const NFTDetails = () => {
                     </div>
 
                     <div className="row">
-                        {/*<div className="col-md-12">*/}
-                        {/*    <br />*/}
-                        {/*    <br />*/}
-                        {/*    <div className="card">*/}
-                        {/*        <div className="card-body">*/}
-                        {/*            <div>*/}
-                        {/*                <Line options={options1} data={IOT} />*/}
-                        {/*            </div>*/}
-                        {/*        </div>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
+                       
                             <div className="col-md-12">
                                 <br />
                                 <br />
@@ -580,6 +617,6 @@ export const NFTDetails = () => {
             <br />
             <br />
         <Footer />
-      </>
+      </React.Fragment>
     )
 }
