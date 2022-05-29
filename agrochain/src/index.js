c77ad911873cf8e1ed3fd2c0ede7c04d3f0d5e5d
Vijay Palaskar <vijaypalaskar@rediffmail.com>
2022-05-29 14:44:46 +0530

    pwa support changes

--- agrochain/src/index.js
+++ agrochain/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import NftProvider from './frontend/NftContext/NftProvider';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,6 +17,11 @@ root.render(
     </NftProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
