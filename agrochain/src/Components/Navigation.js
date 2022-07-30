db70cd50d8a60d46a776078767f0310b4fc09913
ARNAB DAS <raj713335@gmail.com>
2022-07-30 21:53:01 +0530

    Revert ":wrench: Adding Unstoppable Domain"

--- agrochain/src/Components/Navigation.js
+++ agrochain/src/Components/Navigation.js
@@ -1,61 +1,18 @@
 import React, { useContext } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom';
 import { NftContext } from '../frontend/NftContext/NftProvider';
-import connectors from './connectors'
-import { useWeb3React } from '@web3-react/core'
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import farmer from './farmer.png'
-import udomain from './udomain.png'
 
 const Navigation = ({ web3Handler }) => {
-    const { accounts, setAccount, accountType, setAccountType } = useContext(NftContext);
-    const { active, account, activate, deactivate } = useWeb3React()
+    const { account, setAccount, accountType, setAccountType } = useContext(NftContext);
     const navigate = useNavigate();
 
     const logout = () => {
         setAccount('')
         setAccountType(false);
-        deactivate()
         localStorage.removeItem('account');
         navigate('/');
     }
-
-    function createConnectHandler(connectorId: string) {
-        return async () => {
-            try {
-                const connector = connectors[connectorId]
-
-                // Taken from https://github.com/NoahZinsmeister/web3-react/issues/124#issuecomment-817631654
-                if (
-                    connector instanceof WalletConnectConnector &&
-                    connector.walletConnectProvider
-                ) {
-                    connector.walletConnectProvider = undefined
-                }
-                console.log(connector.walletConnectProvider)
-                await activate(connector)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-    }
-
-    //async function handleDisconnect() {
-    //    try {
-    //        deactivate()
-    //    } catch (error) {
-    //        console.error(error)
-    //    }
-    //}
-
-    //if (active) {
-    //    return (
-    //        <>
-    //            <div>Connected to {account}</div>
-    //            <button onClick={handleDisconnect}>Disconnect</button>
-    //        </>
-    //    )
-    //}
 
     return (
         <nav className="navbar navbar-expand-lg navbar-effects sticky-top">
@@ -86,12 +43,7 @@ const Navigation = ({ web3Handler }) => {
                         {account ? (<><button className="nav-btn btn btn-secondary btn-sm btn-block"><a href={`https://rinkeby.etherscan.io/address/${account}`} target="_blank"
                             rel="noopener noreferrer" > {account.slice(0, 5) + '...' + account.slice(38, 42)}</a></button>
                             <button onClick={logout} className="nav-btn btn btn-danger btn-sm btn-block ms-2"><i className="fa fa-sign-out-alt fa-fw"></i> Disconnect Wallet </button></>) : (
-                                <><>{Object.keys(connectors).map(v => (
-                                    <><button className="nav-btn btn btn-primary btn-sm btn-block ms-2" key={v} onClick={createConnectHandler(v)}>
-                                        Login with Unstoppable
-                                    </button></>
-                                ))}<img src={udomain} className="ms-2 mt-2" style={{ width: "38px" }} /></></>
-                                                         )}
+                            <button onClick={web3Handler} className="nav-btn btn btn-success btn-sm btn-block ms-2"><i className="fa fa-user-plus fa-fw"></i> Connect Wallet </button>)}
                     </li>
                 </ul>
             </div>
