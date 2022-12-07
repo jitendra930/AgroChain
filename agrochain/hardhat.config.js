78b4ee4b4e39e324dee684d9f832ba23eef66a90
ARNAB DAS <raj713335@gmail.com>
2022-12-08 03:21:59 +0530

    :wrench: Updated the hardhat.config.js

--- agrochain/hardhat.config.js
+++ agrochain/hardhat.config.js
@@ -5,12 +5,12 @@ const PRIVATE_KEY = '';
 
 module.exports = {
     solidity: "0.8.4",
-    networks: {
-        goerli: {
-            url: INFURA_URL,
-            accounts: [`0x${PRIVATE_KEY}`]
-        }
-    },
+    //networks: {
+    //    goerli: {
+    //        url: INFURA_URL,
+    //        accounts: [`0x${PRIVATE_KEY}`]
+    //    }
+    //},
     paths: {
     artifacts: "./src/backend/artifacts",
     sources: "./src/backend/contracts",
